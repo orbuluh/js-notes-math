0634d657aa82e496498b11896d9d0552d17bad29
orbuluh <scuendless@gmail.com>
2026-03-02 14:40:24 +0800

    Flatten learning goals into top-level MDX pages

--- content/_meta.ts
+++ content/_meta.ts
@@ -4,5 +4,23 @@ export default {
   },
   'learning-goals': {
     title: 'Learning Goals'
+  },
+  'probability-and-statistics': {
+    title: 'Probability and Statistics'
+  },
+  'linear-algebra': {
+    title: 'Linear Algebra'
+  },
+  'time-series-econometrics': {
+    title: 'Time-Series and Econometrics'
+  },
+  'stochastic-processes-dynamic-systems': {
+    title: 'Stochastic Processes and Dynamic Systems'
+  },
+  'calculus-optimization': {
+    title: 'Calculus and Optimization'
+  },
+  'numerical-methods-computation': {
+    title: 'Numerical Methods and Computation'
   }
 }
